a9c7bc4394fe0de5bd18721c40e00cdba8cc5f56
jonesj <justin.jones@lumiinsight.com>
2017-03-03 10:57:55 +0000

    Deleted some unused files. JQuery and SignalR not bundled.

--- Mvc5Angular2SignalRWithWebpack/src/app/app.module.ts
+++ Mvc5Angular2SignalRWithWebpack/src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
-import { GithubService } from './github/shared/github.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
-
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
+
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 import { ChatComponent } from './components/chat/chat.component';
 import { SignalRService } from './services/signalr.service';
@@ -23,11 +19,7 @@ import { SignalRService } from './services/signalr.service';
 	declarations: [
 		AppComponent,
 		AboutComponent,
-		RepoBrowserComponent,
-		RepoListComponent,
-		RepoDetailComponent,
 		HomeComponent,
-		ContactComponent,
 		ChatComponent
 	],
 	imports: [
@@ -38,7 +30,6 @@ import { SignalRService } from './services/signalr.service';
 		RouterModule.forRoot(rootRouterConfig, { useHash: true })
 	],
 	providers: [
-		GithubService,
 		SignalRService
 	],
 	bootstrap: [AppComponent]
